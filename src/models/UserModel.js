2656bc1c2dcdeade5120fe4079e9a755825e8945
nithyanand <pagidinithyanand@gmail.com>
2023-10-21 19:59:29 +0530

    refactor: add models

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
-  email: { type: String, unique: true, required: true },
+  email: { type: String, required: true },
   profile_pic: { type: Buffer },
   hash: { type: String, required: true },
   salt: { type: String, required: true }
